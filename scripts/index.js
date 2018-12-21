192eaf9cff291e926021a25c29441b57ef30e34d
its-gio <me@its-g.io>
2018-12-21 15:34:49 -0800

    New options added to country drop down

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,26 @@
+// Fill in form
+const countryDrop = document.getElementById("country");
+const citiesDrop = document.getElementById("cities");
+let localData;
+
+(async function getData() {
+  await fetch("../data/data.json")
+    .then(blob => blob.json())
+    .then(data => (localData = data));
+  locationPick(localData);
+})();
+
+function locationPick(localData) {
+  const countries = Object.keys(localData);
+
+  countries.forEach(country => {
+    let option = document.createElement("option");
+    option.text = `${country}`;
+    countryDrop.appendChild(option);
+  });
+}
+
+// Tabs functionality
 function openTab(e, tabName) {
   const tabInfo = document.getElementsByClassName("tab-info");
   const tablinks = document.getElementsByClassName("tablinks");
